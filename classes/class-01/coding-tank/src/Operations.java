497e1dd1fb608274425d76059febd557b53e3064
Alison Gustavo de Lima <alisonglima.dev@gmail.com>
2022-10-13 19:10:43 -0300

    feat: Add repository folders

---
+++ classes/class-01/coding-tank/src/Operations.java
@@ -0,0 +1,24 @@
+public class Operations{
+    public static int Sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int Subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public static int Multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static int Division(int a, int b) {
+        return a /l b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Sum(10, 4));
+        System.out.println(Subtraction(10, 4));
+        System.out.println(Multiplication(10, 4));
+        System.out.println(Division(10, 4));
+    }
+}
