011ccc916f12aab1ba68bbb3b2731b60259c4578
Alison Gustavo de Lima <alisonglima.dev@gmail.com>
2022-10-13 19:19:59 -0300

    feat: Add class 02 files

---
+++ classes/class-02/Operadores/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String str = "";
+        int a = 10;
+        int b = 2;
+        var c = 3;
+        System.out.println(a + " e " + b);
+        System.out.println("Soma: " + (a + b));
+        System.out.println("Subtração: " + (a - b));
+        System.out.println("Multiplicação: " + (a * b));
+        System.out.println("Divisão: " + (a / b));
+    }
+}
